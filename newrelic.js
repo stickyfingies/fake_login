097795e2d1006aabdfefae06fbf91861cffc776f
Nate <gol504738@student.lths.net>
2017-07-12 16:03:36 +0000

    privacy_update

---
+++ newrelic.js
@@ -0,0 +1,26 @@
+'use strict'
+
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.default.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name: ['Windows 7 Spoof'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key: '1937c184cda584d1ed3f24b2b03dfda71a385023',
+  logging: {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level: 'info'
+  }
+}
